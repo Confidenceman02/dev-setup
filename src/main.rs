65ddd6e2020e71379899ac8004cb63d4096cb2b3
Jaime Terreu <jaime@Jaimes-MBP.lan>
2023-08-15 00:03:26 +0930

    Download tarball

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use directories::{self, BaseDirs};
 use paris::Logger;
+use std::io::{self, copy};
 use std::path::Path;
 use std::{fs, vec};
+use tempdir::TempDir;
 
 #[derive(Debug, Copy, Clone)]
 enum Directory<'a> {
     Alacritty(&'a Path),
     Nvim(&'a Path),
     Lua(&'a Path),
+    Autoload(&'a Path),
 }
 
 impl Directory<'_> {
@@ -16,11 +19,13 @@ impl Directory<'_> {
             Directory::Alacritty(p) => p,
             Directory::Nvim(p) => p,
             Directory::Lua(p) => p,
+            Directory::Autoload(p) => p,
         }
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let base_dirs = directories::BaseDirs::new().unwrap();
 
     // All required paths
@@ -28,6 +33,7 @@ fn main() {
         Directory::Alacritty(Path::new(".config/alacritty")),
         Directory::Nvim(Path::new(".config/nvim")),
         Directory::Lua(Path::new(".config/nvim/lua")),
+        Directory::Autoload(Path::new(".config/nvim/autoload")),
     ];
 
     let mut log1: Logger = Logger::new();
@@ -35,6 +41,7 @@ fn main() {
 
     let mut log2: Logger = Logger::new();
     create_dirs(validated_dirs, &mut log2);
+    download_tarballs().await.ok();
 }
 
 fn create_dirs<'a>(
@@ -80,4 +87,42 @@ fn check_dirs<'a>(
         }
     }
     checked
+}
+
+async fn download_tarballs() -> Result<(), io::Error> {
+    // Create a directory inside of `std::env::temp_dir()`, named with
+    // the prefix "example".
+    let tmp_dir = TempDir::new("rug-cache")?;
+    let target = "https://github.com/lewis6991/gitsigns.nvim/archive/refs/heads/main.tar.gz";
+    let response = reqwest::get(target).await.unwrap();
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        println!("file downloaded: '{}'", fname);
+        let fname = tmp_dir.path().join(fname);
+        println!("Will be located under {:?}", fname);
+        fs::File::create(fname)?
+    };
+
+    let content = response.text().await.unwrap();
+    println!("Length of this string {}", content.len());
+    copy(&mut content.as_bytes(), &mut dest).unwrap();
+
+    let file_path = tmp_dir.path().join("my-temporary-note.txt");
+    let tmp_file = fs::File::create(file_path)?;
+
+    // By closing the `TempDir` explicitly, we can check that it has
+    // been deleted successfully. If we don't close it explicitly,
+    // the directory will still be deleted when `tmp_dir` goes out
+    // of scope, but we won't know whether deleting the directory
+    // succeeded.
+    drop(tmp_file);
+    tmp_dir.close()?;
+    Ok(())
 }
